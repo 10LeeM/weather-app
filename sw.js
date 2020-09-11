a3340d215db4a19943ddc48680f8b172f813ee47
Nelly Chelaa <40514095+10LeeM@users.noreply.github.com>
2020-09-11 01:14:29 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,32 @@
-self.addEventListener("Install", e => {
+//File-list to cache
+
+const staticAssets=[
+    '/',
+    '/images/weather.png',
+    '/src/app.js',
+    '/src/index.js',
+    '/src/style.css',
+    '/index.html'
+
+];
+
+self.addEventListener("Install", async e => {
     e.waitUntil(
         caches.open('static').then(cache => {
-            return cache.addAll([
-                'app.js',
-                'sw.js',
-                'manifest.js',
-                'style.css'
-            ])
+            return cache.addAll(staticAssets)
         })
+        .catch(console.error)
+
     )
-})
+});
+
+self.addEventListener("fetch", function(event) {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        if (response) {
+          return response; 
+        }
+        return fetch(event.request);
+      })
+    );
+  });
